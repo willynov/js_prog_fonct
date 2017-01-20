f6634ea06e2d2a25136b2494e35cec7b41911979
GALBY <william.galby@cgi.com>
2017-01-20 14:17:58 +0100

    Ajout d'un test de panier

--- testpanier.js
+++ testpanier.js
@@ -13,4 +13,10 @@ describe ('summarizeBasket', function() {
         expect(res.countArticles).to.be.equal(0);
         expect(res.countProducts).to.be.equal(0);
     });
+    it('Basket with only product should return price of the product', function() {
+        var res = summerizeBasket({prices}, ['cucumber']);
+        expect(res.prices).to.be.equal(4);
+        expect(res.countArticles).to.be.equal(1);
+        expect(res.countProducts).to.be.equal(1);
+    });
 });
