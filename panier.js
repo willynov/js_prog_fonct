d54d72319e96c46baefc5141e45df2aabf4ccd80
GALBY <william.galby@cgi.com>
2017-01-20 14:11:45 +0100

    Ajout des fichiers pour test unitaire panier

---
+++ panier.js
@@ -0,0 +1,15 @@
+let summarizeBasket = (prices, products) => {
+    if(!products || !products.length) {
+        return {
+            price : 0,
+            countArticles: 0,
+            countProducts: 0
+        };
+    } else {
+            return 'TO BE CONTINUED';
+    }
+}
+
+module.export = {
+    summarizeBasket: summarizeBasket
+};
---
+++ testpanier.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+var summerizeBasket = require('./panier.js').summarizeBasket;
+
+let prices = { banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 };
+
+let products = [ 'tomato', 'cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato',
+'potato', 'tomato', 'potato' ];
+
+describe ('summarizeBasket', function() {
+    it('Empty basket should retun 0 for each value', function() {
+        var res = summerizeBasket({}, []);
+        expect(res.prices).to.be.equal(0);
+        expect(res.countArticles).to.be.equal(0);
+        expect(res.countProducts).to.be.equal(0);
+    });
+});
